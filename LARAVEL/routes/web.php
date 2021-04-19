332c5bb9ebe082af5290b6eb1303f6fe51fd7348
MLutfiF <71967862+MLutfiF@users.noreply.github.com>
2021-04-18 17:18:08 -0700

    Update

--- LARAVEL/routes/web.php
+++ LARAVEL/routes/web.php
@@ -15,3 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('user', 'ManagementUserController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin/profile', function ($id) {
+    //
+})->middleware(CheckAge::class);
+
+Route::get('/', function ($id) {
+    //
+})->middleware('web');
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
+
+Route::middleware(['web', 'subcsribed'])->group(function () {
+    //
+});
+
+Route::put('post/{id}', function ($id) {
+    //
+})->middleware('role:editor');
